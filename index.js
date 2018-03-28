4aeca7e0e430b363dee70c497eabd10f04146598
Dmitry <ecio1476@gmail.com>
2018-03-28 14:44:13 +0800

    Initial commit

---
+++ index.js
@@ -0,0 +1,10 @@
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send({ hi: 'there'});
+});
+
+const POST = process.env.PORT || 5000;
+app.listen(PORT);
