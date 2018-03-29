4967508e7dbe9ddd2e4a1170e8d4dcff5da8fe59
Dmitry <ecio1476@gmail.com>
2018-03-29 14:36:20 +0800

    Add nodemon to the project

--- index.js
+++ index.js
@@ -12,8 +12,11 @@ passport.use(
             clientSecret: keys.googleClientSecret,
             callbackURL: "/auth/google/callback"
         },
-        accessToken => {
-            console.log(accessToken);
+        (accessToken, refreshToken, profile, done) => {
+            console.log("Access Token: ", accessToken);
+            console.log("Refresh Token: ", refreshToken);
+            console.log("Profile: ", profile);
+            console.log("Done: ", done);
         }
     )
 );
