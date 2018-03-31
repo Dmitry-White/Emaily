378b28c774cc3010122bb08daaf1c1af6075c17e
Dmitry <ecio1476@gmail.com>
2018-03-31 13:04:16 +0800

    Add conditional callbacks to the pasport promise

--- services/passport.js
+++ services/passport.js
@@ -14,7 +14,18 @@ passport.use(
             callbackURL: "/auth/google/callback"
         },
         (accessToken, refreshToken, profile, done) => {
-            new User({googleId: profile.id}).save();
+            User.findOne({googleId: profile.id})
+                .then(existingUser => {
+                    if (existingUser) {
+                        done(null, existingUser);
+                    } else {
+                        new User({googleId: profile.id})
+                            .save()
+                            .then(user => {
+                                done(null, user);
+                            });
+                    }
+                });
         }
     )
 );
