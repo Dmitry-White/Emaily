196e3618a10f62634935ebbed59f8d3db5e44a3f
Dmitry <ecio1476@gmail.com>
2018-04-02 12:27:57 +0800

    Add few dummy components

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,10 +1,18 @@
 import React from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
+
+const Header = () => <h2>Header</h2>;
+const Dashboard = () => <h2>Dashboard</h2>;
+const SurveyNew = () => <h2>SurveyNew</h2>;
+const Landing = () => <h2>Landing</h2>;
 
 const app = () => {
     return (
-        <div>
-            Hi There!
-        </div>
+        <BrowserRouter>
+            <div>
+                Hi There!
+            </div>
+        </BrowserRouter>
     );
 };
 
