036eb5758615b7d081730ac26e17b505941c7826
Dmitry <ecio1476@gmail.com>
2018-04-03 14:29:13 +0800

    Add navbar and styles

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -8,7 +8,7 @@ const Landing = () => <h2>Landing</h2>;
 
 const app = () => {
     return (
-        <div>
+        <div className="container">
             <BrowserRouter>
                 <div>
                     <Header/>
--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,11 +1,19 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 
 class Header extends Component {
     render() {
         return (
-            <div>
-                Header
-            </div>
+            <nav>
+                <div className="nav-wrapper">
+                  <Link to="/" className="left brand-logo">Emaily</Link>
+                  <ul className="right">
+                    <li><Link to="/surveys">Surveys</Link></li>
+                    <li><Link to="/surveys/new">New Survey</Link></li>
+                    <li><a href="/auth/google">Login With Google</a></li>
+                  </ul>
+                </div>
+            </nav>
         );
     };
 };
