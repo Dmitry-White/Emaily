6ab90478d68a41331d25fed35cdfb72cf09280b3
Dmitry <ecio1476@gmail.com>
2018-04-08 23:20:33 +0800

    Remove unused nav link

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -22,7 +22,6 @@ class App extends Component {
                         <Header/>
                         <Route exact path="/" component={Landing}/>
                         <Route exact path="/surveys" component={Dashboard}/>
-                        <Route path="/surveys/new" component={SurveyNew}/>
                     </div>
                 </BrowserRouter>
             </div>
