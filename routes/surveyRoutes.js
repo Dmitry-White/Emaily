88f6c7458f1d3024da60980e9c2ba80b17115377
Dmitry <ecio1476@gmail.com>
2018-04-10 17:39:24 +0800

    Fix typos in Mailer

--- routes/surveyRoutes.js
+++ routes/surveyRoutes.js
@@ -22,6 +22,7 @@ module.exports = app => {
                 dateSent: Date.now()
             });
             const mailer = new Mailer(survey, surveyTemplate(survey));
+            mailer.send();
         }
     );
 };
--- services/Mailer.js
+++ services/Mailer.js
@@ -7,6 +7,8 @@ const keys = require('../config/keys');
 class Mailer extends helper.Mail {
     constructor({ subject, recipients }, content) {
         super();
+
+        this.sgApi = sendgrid(keys.sendGridKey);
         this.from_email = new helper.Email('no-reply@email.com');
         this.subject = subject;
         this.body = new helper.Content('text/html', content);
@@ -36,6 +38,16 @@ class Mailer extends helper.Mail {
             personalize.addTo(recipient);
         });
         this.addPersonalization(personalize);
+    };
+
+    async send() {
+        const request = this.sgApi.emptyRequest({
+            method: 'POST',
+            path: '/v3/mail/send',
+            body: this.toJSON()
+        });
+        const response = this.sgApi.API(request);
+        return response;
     };
 };
 
