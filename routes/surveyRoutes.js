fb9a670fff059f2207149bd4b1d644ace08aaa3c
Dmitry <ecio1476@gmail.com>
2018-04-10 18:18:50 +0800

    Add polishing touches to my awesome backend ;)

--- routes/surveyRoutes.js
+++ routes/surveyRoutes.js
@@ -7,11 +7,14 @@ const surveyTemplate = require('../services/emailTemplates/surveyTemplate');
 const Survey = mongoose.model('surveys');
 
 module.exports = app => {
+    app.get('/api/surveys/thanks', (req,res) => {
+        res.send("Thanks for voting!");
+    });
     app.post(
         '/api/surveys',
         requireLogin,
         requireCredits,
-        (req, res) => {
+        async (req, res) => {
             const {title, subject, body, recipients} = req.body;
             const survey = new Survey({
                 title,
@@ -21,8 +24,17 @@ module.exports = app => {
                 _user: req.user.id,
                 dateSent: Date.now()
             });
+
             const mailer = new Mailer(survey, surveyTemplate(survey));
-            mailer.send();
+            try{
+                await mailer.send();
+                await survey.save();
+                req.user.credits -= 1;
+                const user = await req.user.save();
+                res.send(user);
+            } catch (err) {
+                res.status(422).send(err);
+            };
         }
     );
 };
