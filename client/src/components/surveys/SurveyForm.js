a656d48d3ca18ed91377061d3157106675119b7d
Dmitry <ecio1476@gmail.com>
2018-04-10 23:26:20 +0800

    Add custom fields

--- client/src/components/surveys/SurveyForm.js
+++ client/src/components/surveys/SurveyForm.js
@@ -1,13 +1,31 @@
+//import _ from 'lodash';
 import React, { Component } from 'react';
-import { reduxForm } from 'redux-form';
-import { connect } from 'react-redux';
+import { reduxForm, Field } from 'redux-form';
+import SurveyField from './SurveyField';
 
-import * as actions from '../../actions';
+const FIELDS = [
+    { label: 'Survey Title', name: 'title' },
+    { label: 'Subject Line', name: 'subject' },
+    { label: 'Email Body', name: 'body' },
+    { label: 'Recipient List', name: 'emails' }
+];
 
 class SurveyForm extends Component {
+    renderFields() {
+        /*return _.map(FIELDS, ({label, name}) => {
+            return <Field key={name} component={SurveyField} type="text" label={label} name={name}/>
+        });*/
+        return FIELDS.map(({label, name}) => {
+            return <Field key={name} component={SurveyField} type="text" label={label} name={name}/>
+        });
+    };
+
     render() {
         return (
-            <h3>Survey Form here! </h3>
+            <form onSubmit={this.props.handleSubmit(values => console.log(values))}>
+                {this.renderFields()}
+                <button type="submit">Submit</button>
+            </form>
         );
     };
 };
