23bb56d97c61dfe09bc863566996ad03c99efcb1
Dmitry <ecio1476@gmail.com>
2018-04-10 13:36:11 +0800

    Add requirements to make a survey

---
+++ routes/surveyRoute.js
@@ -0,0 +1,13 @@
+const requireLogin = require('../middlewares/requireLogin');
+const requireCredits = require('../middlewares/requireCredits');
+
+module.exports = app => {
+    app.post(
+        '/api/surveys',
+        requireLogin,
+        requireCredits
+        (req, res) => {
+
+        }
+    );
+};
