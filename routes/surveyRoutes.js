a7c28d976ef804f17ae5d34d967cac9d38fd0421
Dmitry <ecio1476@gmail.com>
2018-04-12 13:19:07 +0800

    Finish webhook

--- routes/surveyRoutes.js
+++ routes/surveyRoutes.js
@@ -10,6 +10,10 @@ const surveyTemplate = require('../services/emailTemplates/surveyTemplate');
 const Survey = mongoose.model('surveys');
 
 module.exports = app => {
+    app.get('/api/surveys/:surveyId/:choice', (req,res) => {
+        res.send('Thanks for voting!');
+    });
+    
     app.post('/api/surveys/webhooks', (req,res) => {
         const p = new Path('/api/surveys/:surveyId/:choice');
         const events = _.chain(req.body)
@@ -21,8 +25,19 @@ module.exports = app => {
             })
             .compact()
             .uniqBy('email', 'surveyId')
+            .each(({surveyId, email, choice}) => {
+                Survey.updateOne({
+                    _id: surveyId,
+                    recipients:{
+                        $elemMatch: { email: email, responded: false }
+                    }
+                }, {
+                    $inc: { [choice]: 1},
+                    $set: { 'recipients.$.responded': true },
+                    lastResponded: new Date()
+                }).exec();
+            })
             .value();
-        console.log(events);
         res.send({});
     });
 
