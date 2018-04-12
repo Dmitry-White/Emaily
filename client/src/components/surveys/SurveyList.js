c3932c0ac0648e8ecab8b5d9a34c6fcd8fbd8789
Dmitry <ecio1476@gmail.com>
2018-04-12 14:18:25 +0800

    Finish SurveyList component

--- client/src/components/surveys/SurveyList.js
+++ client/src/components/surveys/SurveyList.js
@@ -8,9 +8,27 @@ class SurveyList extends Component {
         this.props.fetchSurveys();
     };
 
+    renderSurveys = () => {
+        return this.props.surveys.reverse().map(survey => {
+            return (
+                <div key={survey._id} className="card darken-1">
+                    <div className="card-content">
+                      <span className="card-title">{survey.title}</span>
+                      <p>{survey.body}</p>
+                      <p className="right">Sent on: {new Date(survey.dateSent).toLocaleDateString()}</p>
+                    </div>
+                    <div className="card-action">
+                      <a>Yes: {survey.yes}</a>
+                      <a>No: {survey.no}</a>
+                    </div>
+                </div>
+            );
+        });
+    };
+
     render() {
         return (
-            <div> SurveyList </div>
+            <div>{this.renderSurveys()}</div>
         );
     };
 };
