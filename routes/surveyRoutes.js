2db0c06cccb4c5a5aba738ce61871d81dd1d29cb
Dmitry <ecio1476@gmail.com>
2018-04-12 12:43:57 +0800

    Refactor events

--- routes/surveyRoutes.js
+++ routes/surveyRoutes.js
@@ -1,3 +1,6 @@
+const _ = require('lodash');
+const { Path } = require('path-parser');
+const { URL } = require('url');
 const mongoose = require('mongoose');
 const requireLogin = require('../middlewares/requireLogin');
 const requireCredits = require('../middlewares/requireCredits');
@@ -7,13 +10,22 @@ const surveyTemplate = require('../services/emailTemplates/surveyTemplate');
 const Survey = mongoose.model('surveys');
 
 module.exports = app => {
-    app.get('/api/surveys/thanks', (req,res) => {
-        res.send("Thanks for voting!");
-    });
     app.post('/api/surveys/webhooks', (req,res) => {
-        console.log(req.body);
+        const p = new Path('/api/surveys/:surveyId/:choice');
+        const events = _.chain(req.body)
+            .map(({email, url}) => {
+                const match = p.test(new URL(url).pathname);
+                if (match) {
+                    return { email, ...match };
+                }
+            })
+            .compact()
+            .uniqBy('email', 'surveyId')
+            .value();
+        console.log(events);
         res.send({});
     });
+
     app.post(
         '/api/surveys',
         requireLogin,
@@ -28,7 +40,7 @@ module.exports = app => {
                 _user: req.user.id,
                 dateSent: Date.now()
             });
-            
+
             const mailer = new Mailer(survey, surveyTemplate(survey));
             try{
                 await mailer.send();
