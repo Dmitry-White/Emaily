8ec8a38c0c83137ef67ac3c9eb7619425b715cdb
Dmitry <ecio1476@gmail.com>
2018-04-12 13:40:10 +0800

    Add get route for list of surveys

--- routes/surveyRoutes.js
+++ routes/surveyRoutes.js
@@ -10,10 +10,18 @@ const surveyTemplate = require('../services/emailTemplates/surveyTemplate');
 const Survey = mongoose.model('surveys');
 
 module.exports = app => {
+    app.get('/api/surveys',
+        requireLogin,
+        async (req, res) => {
+            const surveys = await Survey.find({ _user: req.user.id});
+            res.send(surveys);
+        }
+    );
+
     app.get('/api/surveys/:surveyId/:choice', (req,res) => {
         res.send('Thanks for voting!');
     });
-    
+
     app.post('/api/surveys/webhooks', (req,res) => {
         const p = new Path('/api/surveys/:surveyId/:choice');
         const events = _.chain(req.body)
