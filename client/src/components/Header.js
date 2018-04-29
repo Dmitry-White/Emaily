68b80725a222eb8ee78705cec227ae84d6cdd99f
Dmitry <ecio1476@gmail.com>
2018-04-29 16:10:00 +0800

    Change logo link

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -64,7 +64,7 @@ class Header extends Component {
         return (
             <nav>
                 <div className="nav-wrapper">
-                  <Link to={this.props.auth ? "/surveys" : "/"} className="left brand-logo">Emaily</Link>
+                  <Link to="/" className="left brand-logo">Emaily</Link>
                   <ul className="right">
                         {this.renderContent()}
                   </ul>
