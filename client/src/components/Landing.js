61dcdce0c3964e20b20fb7e0b6122f93411f8f83
Dmitry <ecio1476@gmail.com>
2018-04-29 16:32:49 +0800

    Add image to the dashboard

--- client/src/components/Landing.js
+++ client/src/components/Landing.js
@@ -1,10 +1,13 @@
 import React from 'react';
 
+const gimme = require('../utils/gimme_feedback.jpg');
+
 const landing = () => {
     return (
         <section style={{textAlign: "center"}}>
             <h1>Emaily!</h1>
             <p>Collect feedback from your users</p>
+            <img src={gimme} style={{width: "70%"}} alt="Gimme feedback"/>
         </section>
     );
 };
